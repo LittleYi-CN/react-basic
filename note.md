6a71adae74fc5b9820b18faaaf0f525c42aa96b4
仪泉江 <sdgmyqj@163.com>
2022-07-28 16:26:02 +0800

    ✨ 4、模板精简原则

--- note.md
+++ note.md
@@ -74,3 +74,35 @@ function App() {
 ```
 
 ## 4、模板精简原则
+- 原则：模板中的逻辑尽量保持精简。
+- 复杂的多分枝的逻辑 收敛为一个函数 通过一个专门的函数来写分支逻辑 模板中只调用函数。
+```
+// 有一个状态type 1 2 3
+// 1 -> h1
+// 2 -> h2
+// 3 -> h3
+// 原则：模板中的逻辑尽量保持精简
+// 复杂的多分枝的逻辑 收敛为一个函数 通过一个专门的函数来写分支逻辑 模板中只调用函数
+const getHTag =(type) => {
+  if(type === 1) {
+    return <h1>this is h1</h1>
+  }
+
+  if(type === 2) {
+    return <h2>this is h2</h2>
+  }
+
+  if(type === 3) {
+    return <h3>this is h3</h3>
+  }
+}
+function App() {
+  return(
+    <div className="App">
+      { getHTag(1) }
+    </div>
+  )
+}
+```
+
+## 5、动态类名控制
--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-// 条件渲染
-// 技术方案：1、三元表达式(常用) 2、逻辑&&运算
+// 有一个状态type 1 2 3
+// 1 -> h1
+// 2 -> h2
+// 3 -> h3
+// 原则：模板中的逻辑尽量保持精简
+// 复杂的多分枝的逻辑 收敛为一个函数 通过一个专门的函数来写分支逻辑 模板中只调用函数
+const getHTag =(type) => {
+  if(type === 1) {
+    return <h1>this is h1</h1>
+  }
 
-// 1、三元表达式 - 满足条件才渲染一个span标签
-// const flag = true;
-// function App() {
-//   return(
-//     <div className="App">
-//       { flag ? (
-//         <div>
-//           <span>this is span</span>
-//         </div>) : null }
-//     </div>
-//   )
-// }
-// 2、逻辑 && 运算
+  if(type === 2) {
+    return <h2>this is h2</h2>
+  }
+
+  if(type === 3) {
+    return <h3>this is h3</h3>
+  }
+}
 function App() {
   return(
     <div className="App">
-      { true && <span>this is another span</span> }
+      { getHTag(1) }
     </div>
   )
 }
