3b4103ead11c3aca8a6da8e7e9da304a2a819bc2
仪泉江 <sdgmyqj@163.com>
2022-08-03 17:40:13 +0800

    ✨ 26、父传子实现

--- note.md
+++ note.md
@@ -832,3 +832,45 @@ function App () {
 3. 其它关系 - mobx / redux / 基于hook的方案
 
 ## 26、父传子实现
+**实现步骤**
+1. 父组件提供要传递的数据 - state
+2. 给子组件标签`添加属性`值为state中的数据
+3. 子组件中通过`props`接收父组件中传过来的数据
+   1. 类组件使用this.props获取props对象
+   2. 函数式组件直接通过参数获取props对象
+
+```
+import React from "react"
+
+// 函数式的son
+function SonF(props) {
+  return (
+    <div>函数子组件, {props.msg}</div>
+  )
+}
+
+// 类组件的son
+class SonC extends React.Component {
+  render() {
+    return (
+      <div>类子组件, {this.props.msg}</div>
+    )
+  }
+}
+
+// App 父组件 Son 子组件
+class App extends React.Component {
+  state = {
+    message: 'this is message'
+  }
+  render() {
+    return (
+      <>
+        {/* 子组件身上绑定属性 属性名可以自定义 保持语义化 */}
+        <SonF msg={this.state.message} />
+        <SonC msg={this.state.message} />
+      </>
+    )
+  }
+}
+```
--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React, { createRef } from "react"
+import React from "react"
 
-class Input extends React.Component {
-  msgRef = createRef()
-  getValue = () => {
-    console.log(this.msgRef.current.value)
+// 函数式的son
+function SonF(props) {
+  return (
+    <div>函数子组件, {props.msg}</div>
+  )
+}
+
+// 类组件的son
+class SonC extends React.Component {
+  render() {
+    return (
+      <div>类子组件, {this.props.msg}</div>
+    )
+  }
+}
+
+// App 父组件 Son 子组件
+class App extends React.Component {
+  state = {
+    message: 'this is message'
   }
   render() {
     return (
       <>
-        <input
-          type='text'
-          ref={this.msgRef}
-          />
-        <div>
-          <button onClick={this.getValue}>点我获取input值</button>
-        </div>
+        {/* 子组件身上绑定属性 属性名可以自定义 保持语义化 */}
+        <SonF msg={this.state.message} />
+        <SonC msg={this.state.message} />
       </>
     )
   }
-}
-function App () {
-  return (
-    <>
-      <Input></Input>
-    </>
-  )
 }
 
 export default App
