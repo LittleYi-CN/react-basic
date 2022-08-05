43ace5aa28a113f1624e2bed7977621bbf68950c
仪泉江 <sdgmyqj@163.com>
2022-08-05 17:42:38 +0800

    ✨ 46、hook-useState-组件的更新过程

--- src/App.js
+++ src/App.js
@@ -15,10 +15,24 @@
 //    setCount（基于原值计算得到的新值）
 // 4. count和setCount是一对 是绑定在一起的 setCount只能用来修改对应的count值
 
+// 组件的更新
+// 当调用setCount的时候更新过程
+// 首次渲染
+// 首次被渲染的时候 组件内部的代码会被执行一次
+// 其中useState也会耕者执行 重点注意 初始值只在首次渲染生效
+
+// 更新渲染 setCount都会更新
+// 1. app组件会再次渲染 这个函数会再次执行
+// 2.useState再次执行 得到的新的count值不是0 而是修改之后的1 模板会用新值渲染
+// 重点一句话：useState初始值只在首次渲染生效后续只要调用setCount整个app中代码都会执行
+// 此时的count每次拿到的都是最新值
+
+
 import {useState} from 'react'
 
 function App() {
   const [count,setCount] = useState(0)
+  console.log(count)
   return (
     <>
       <button onClick={() => setCount(count + 1)}>{count}</button>
