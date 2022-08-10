2bfca7acf334e48807e5827a3e1f0f2a0456d7f4
仪泉江 <sdgmyqj@163.com>
2022-08-10 16:35:53 +0800

    ✨ 57、react-router-最小化demo实现

--- note.md
+++ note.md
@@ -2147,3 +2147,31 @@ function App() {
   - 补充
     - 1. 如果提供数据是静态不变 - index.js包裹
     - 2. 如果提供过的数据需要变化 - app.js包裹
+
+## 57、react-router-最小化demo实现
+```
+npm install react-router-dom@6
+```
+```
+import About from "./About"
+import Home from "./Home"
+// 进行路由配置
+import { BrowserRouter, Link, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    // 声明当前要用一个非hash模式的路由
+    <BrowserRouter>
+      {/* 指定跳转的组件 to用来配置路由地址 */}
+      <Link to="/">首页</Link>
+      <Link to="/about">关于</Link>
+      {/* 路由出口 路由对应的组件会在这里进行渲染 */}
+      <Routes>
+        {/* 指定路径和组件的对应关系 path代表路径 element代表组件 成对出现 path -> element */}
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+```
--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import { useContext } from 'react'
-
-import Context from './context'
-function ComA() {
-  const count = useContext(Context)
-  return (
-    <>
-      <div>this is A</div>
-      app传过来的数据为： {count}
-      <ComC />
-    </>
-  )
-}
-
-function ComC() {
-  const count = useContext(Context)
-  return (
-    <>
-      <div>this is C</div>
-      app传过来的数据为： {count}
-    </>
-  )
-}
+import About from "./About"
+import Home from "./Home"
+// 进行路由配置
+import { BrowserRouter, Link, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-      <ComA />
+    // 声明当前要用一个非hash模式的路由
+    <BrowserRouter>
+      {/* 指定跳转的组件 to用来配置路由地址 */}
+      <Link to="/">首页</Link>
+      <Link to="/about">关于</Link>
+      {/* 路由出口 路由对应的组件会在这里进行渲染 */}
+      <Routes>
+        {/* 指定路径和组件的对应关系 path代表路径 element代表组件 成对出现 path -> element */}
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
